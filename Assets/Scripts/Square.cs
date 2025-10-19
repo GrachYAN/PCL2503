b211739dc04a358089f75bc41b6308ced0ad092a
GrachYAN <grachyyt@gmail.com>
2025-10-19 16:00:29 +0800

    temp boad

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Square : MonoBehaviour
 {
-    public int x, y; 
+    public int x;
+    public int y;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position + Vector3.up * 0.01f, new Vector3(0.95f, 0.01f, 0.95f));
+    }
+    //ÏÔÊ¾¸ñ×Ó±ß¿ò
 }
